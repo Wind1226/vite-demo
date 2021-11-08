75308a87c8717ed61ce5ba0381e947d439de96c6
Administrator <admin@example.com>
2021-11-08 14:59:49 +0800

    upd project init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+defineProps({
+  msg: String
+})
+
+const data = ref('121');
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <input type="button" :value="`click me ${data}`" @click="data += 1" />
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
